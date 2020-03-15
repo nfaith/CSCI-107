8f3e7b5b3e29a918e1afbcf3d9c9f96deb52b1c9
nfaith <49287441+nfaith@users.noreply.github.com>
2020-03-15 11:08:23 -0600

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+
+print("+------------------------------------------------+")
+print("|    |                                           |")
+print("|   -|          Nelson, Faith                     |")
+print("|  --|          Tribute Liabilities Associate    |")
+print("| ---|          Parasail Capital                 |")
+print("| ---------                                      |")
+print("|  -------      4 Hunger Plaza                   |")
+print("|               STE 1400                         |")
+print("|               District 12, Panem 00012         |")
+print("|                                                |")
+print("| Work: (406)-587-2248  @:faith@parasail.com     |")
+print("+------------------------------------------------+")
+
+
